5730b3be46f0b43e0a30512ee3d1083deb88da2f
EgoInc <56895607+EgoInc@users.noreply.github.com>
2022-04-16 15:44:17 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ import setuptools
 
 setuptools.setup(
     name='py_etherscan_api',
-    version='0.9.0',
+    version='0.9.1',
     packages=['examples', 'examples.stats', 'examples.tokens',
               'examples.accounts', 'examples.blocks', 'examples.transactions',  'etherscan'],
-    url='https://github.com/corpetty/py-etherscan-api',
+    url='https://github.com/EgoInc/py-etherscan-api',
     license='MIT',
     author='coreypetty',
-    author_email='petty.btc@gmail.com',
+    author_email='sex.btc@gmail.com',
     description='Python Bindings to Etherscan.io API',
     install_requires=[
         'requests>=2.20.0',
